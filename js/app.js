6c1d9aea2a6bee1637a1c0f49bab1012b192dd56
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-30 18:48:22 +0200

    added name litst in js

--- js/app.js
+++ js/app.js
@@ -18,3 +18,33 @@ $(window).bind('mousewheel', function(event) {
 
     }
 });
+
+
+var Team = function(name, imgURL) {
+  this.name = name,
+  this.imgURL = imgURL,
+  this.bio = bio
+};
+
+var stefan = new Team("Stefan Nedelcu", "img/people/stefan_n.jpg");
+var matei = new Team("Matei Vlasceanu", "img/people/matei_v.jpg");
+var nana = new Team("Mariana Borla", "img/people/nana_b.jpg");
+var radu = new Team("Radu Barota", "img/people/radu_b.jpg");
+var bogdanM = new Team("Bogdan Mihaila", "img/people/bogdan_m.jpg");
+var ciprian = new Team("Ciprian Rasoiu", "img/people/ciprian_r.jpg");
+var bogdanG = new Team("Bogdan Gogoci", "img/people/bogdan_g.jpg");
+var daniel = new Team("Daniel Nedelcu", "img/people/daniel_n.jpg");
+
+var team = [stefan, matei, nana, radu, bogdanM, ciprian, bogdanG, daniel];
+
+var HTMLteamPic = '<img class="img responsive col-lg-8 p-pic col-centered" src="%data%">';
+var HTMLnamesList = '<li class="p-name">%data%</li>';
+
+var formattedPic, formattedNamesList;
+for (var i = 0; i < team.length; i++) {
+  formattedPic = HTMLteamPic.replace("%data%", team[i].imgURL);
+  formattedNamesList = HTMLnamesList.replace("%data%", team[i].name);
+
+  $(".p-names-list").append(formattedNamesList);
+
+}
